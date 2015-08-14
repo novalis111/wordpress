0df42883c77235be367e746dda517a56857da0ab
Ingo Weseloh <it@exanto.de>
2015-08-14 11:38:12 +0200

    Relative Symlinks are only in coreutils >= 8.16

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cp wordpress/index.php .
 sed -i 's/\/wp-blog-header/\/wordpress\/wp-blog-header/g' index.php
-rm -rf wordpress/wp-content && ln -rs wp-content wordpress/
+rm -rf wordpress/wp-content && ln -s ../wp-content wordpress/wp-content
